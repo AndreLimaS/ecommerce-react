6a5bd8525340691c23848088e87f979e9a385712
AndreLimaS <a.s.l0@hotmail.com>
2020-11-04 12:57:35 -0300

    feat: add struct shopping cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,6 +1,55 @@
 import React from "react";
-import { Container } from "./styles";
+import {
+  Container,
+  ListPodruct,
+  CardDetails,
+  LabelNumber,
+  Title,
+} from "./styles";
+import CloseIcon from "@material-ui/icons/Close";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import picProduct1 from "../../assets/m1.jpg";
 
 export default function Cart() {
-  return <Container>Cart</Container>;
+  const [qtd, setQtd] = React.useState(1);
+  const [price, setPrice] = React.useState(300);
+
+  return (
+    <>
+      <Container>
+        <Title>
+          <h1>Shopping Cart</h1>
+        </Title>
+        <ListPodruct>
+          <img src={picProduct1} alt="" />
+          <ul>
+            <li>
+              <h3>Mochila</h3>
+            </li>
+            <li>
+              <p>#434243</p>
+            </li>
+          </ul>
+          <LabelNumber>
+            <RemoveIcon
+              style={{ cursor: "pointer" }}
+              onClick={() => setQtd(qtd - 1)}
+            />
+            <p>{qtd}</p>
+            <AddIcon
+              disabled
+              style={{ cursor: "pointer" }}
+              onClick={() => setQtd(qtd + 1)}
+            />
+          </LabelNumber>
+          <span>R$ {price * qtd}</span>
+          <CloseIcon style={{ cursor: "pointer" }} />
+        </ListPodruct>
+        <CardDetails>
+          <h3>Card Type</h3>
+        </CardDetails>
+      </Container>
+    </>
+  );
 }
